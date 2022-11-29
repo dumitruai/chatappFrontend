2c03c7d9f4e9a4214c25593c9c91dc59a1acc938
dantohi <dantohi@inthergroup.com>
2022-11-29 23:37:02 +0200

    Initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+
+import {Component, OnInit} from '@angular/core';
+import Pusher from 'pusher-js';
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'chatappFrontend';
+export class AppComponent implements OnInit {
+  username = '';
+  message = '';
+  messages = [];
+
+  constructor(private http: HttpClient) {
+  }
+
+  ngOnInit(): void {
+    Pusher.logToConsole = true;
+
+    const pusher = new Pusher('84fcf6a34f3c45cbc922', {
+      cluster: 'eu'
+    });
+
+    const channel = pusher.subscribe('chat');
+    channel.bind('message', data => {
+      this.messages.push(data);
+    });
+  }
+
+  submit(): void {
+    this.http.post('http://localhost:8000/api/messages', {
+      username: this.username,
+      message: this.message
+    }).subscribe(() => this.message = '');
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatCardModule} from "@angular/material/card";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatInputModule} from "@angular/material/input";
+import {FormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
+import {MatButtonModule} from "@angular/material/button";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    HttpClientModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
